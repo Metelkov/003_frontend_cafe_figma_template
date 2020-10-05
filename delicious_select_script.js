4702e66e93a2ef1b18d8aa4c14c519fec1497f3e
metelkov <al.metelkov@gmail.com>
2020-10-05 19:58:28 +0300

    start js

---
+++ delicious_select_script.js
@@ -0,0 +1,30 @@
+function visible__soupe() {
+
+    document.getElementsByClassName('delicious-menu-selector-soupe').style.color ="red";
+}
+
+
+function visible__pizza () {
+   
+}
+
+function visible__pasta () {
+    document.getElementsByClassName('delicious-menu-selector-soupe').style.color ="red";
+
+}
+
+function visible__desert () {
+
+}
+
+function visible__wine () {
+
+}
+
+function visible__beer () {
+
+}
+
+function visible__drinks () {
+
+}
