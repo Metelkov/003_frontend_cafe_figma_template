f6c365b8952d9a0cb04386153a09a037746770fe
Alexey Avdeev <aad.jerry@gmail.com>
2020-10-31 20:10:36 +0300

    feat: update gallery_change.js

--- gallery_change.js
+++ gallery_change.js
@@ -1,26 +1,18 @@
-
-
 /* случайное число - относительно каждой картинки - всего нужно 4 генератора */
-
 let first__part__pic = "no-repeat center url(event_bottom_0";
 let end__part_pic = ".jpg)";
 
 function random__pic() {
-let rnd__part = Math.floor(Math.random() * 4);
+  let rnd__part = Math.floor(Math.random() * 4);
   if (rnd__part == 0) {
     rnd__part++;
   }
-let random__part__pic = rnd__part;
-let gall__pic = first__part__pic+random__part__pic+end__part_pic;
-document.getElementsByClassName('events-gallery__pic02')[0].style.background=gall__pic;
+  let random__part__pic = rnd__part;
+  let gall__pic = first__part__pic + random__part__pic + end__part_pic;
+  document.getElementsByClassName('events-gallery__pic02')[0].style.background = gall__pic;
 }
 
-
-
-
-while (true) {
-  setTimeout(random__pic(), 2000);
-}
+setInterval(random__pic, 2000);
 
 /*  нужно 4 функции, чтобы работали на все картинки
 while (true) {
