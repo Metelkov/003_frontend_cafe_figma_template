479e6c3eb5e380c9a940c8395c3c9bb2913f7f1d
metelkov <al.metelkov@gmail.com>
2020-11-11 18:02:47 +0300

    was fixed

--- sliser.js
+++ sliser.js
@@ -3,51 +3,41 @@ document.getElementsByClassName('slide-00')[0].style.display="flex";
 document.getElementsByClassName('slide-01')[0].style.display="none";
 document.getElementsByClassName('slide-02')[0].style.display="none";
 document.getElementsByClassName('slide-03')[0].style.display="none";
-
-
 */
-
-/*
-let slide__pic-00 = "no-repeat center/112% url(specialties-rectangle.jpg)";
-let slide__pic-01 = "no-repeat center/112% url(chef.jpg)";
-let slide__pic-02 = "no-repeat center/112% url(about-img.jpg)";
-let slide__pic-03 = "no-repeat center/112% url(about-bookatable-img.jpg)";
-let intermediate__slide = "slide__pic-0";
-
-*/
-
 
 //document.getElementsByClassName('specialties-photo slide-01')[0].style.display=slide__pic-00;
 
+let count__sliders=0;
+function next__slide() {
+  count__sliders++;
 
-//let first__part__pic = "no-repeat center url(event_bottom_0";
-//let end__part_pic = ".jpg)";
-
-let slide__part = 0;
-function turn__slide() {
-document.getElementsByClassName('slide-'+slide__part)[0].style.display="flex";
-  slide__part ++
-
-  if (slide__part = 4) {
-    slide__part == 0;
-  }
+if (count__sliders == 1) {
+  //document.getElementsByClassName('slide-00')[0].style.display='flex';
+  document.getElementsByClassName('slide-00')[0].style.left='1440px';
+  document.getElementsByClassName('slide-01')[0].style.display='none';
+  document.getElementsByClassName('slide-02')[0].style.display='none';
+  document.getElementsByClassName('slide-03')[0].style.display='none';
 }
 
-setInterval(turn__slide, 2000);
-
-
-//intermediate
-/* let first__part__pic = "no-repeat center url(event_bottom_0";
-let end__part_pic = ".jpg)";
-
-function random__pic() {
-let rnd__part = Math.floor(Math.random() * 4);
-  if (rnd__part == 0) {
-    rnd__part++;
-  }
-let random__part__pic = rnd__part;
-let gall__pic = first__part__pic+random__part__pic+end__part_pic;
-document.getElementsByClassName('events-gallery__pic02')[0].style.background=gall__pic;
+if (count__sliders == 2) {
+  document.getElementsByClassName('slide-00')[0].style.display='none';
+  //document.getElementsByClassName('slide-01')[0].style.display='flex';
+  document.getElementsByClassName('slide-01')[0].style.left='1440px';
+  document.getElementsByClassName('slide-02')[0].style.display='none';
+  document.getElementsByClassName('slide-03')[0].style.display='none';
 }
 
-*/
+if (count__sliders == 3) {
+  document.getElementsByClassName('slide-00')[0].style.display='none';
+  document.getElementsByClassName('slide-01')[0].style.display='none';
+  //document.getElementsByClassName('slide-02')[0].style.display='flex';
+  document.getElementsByClassName('slide-02')[0].style.left='1440px';
+  document.getElementsByClassName('slide-03')[0].style.display='none';
+}
+
+if (count__sliders == 5) {
+  count__sliders=1; }
+console.log(count__sliders);
+}
+setInterval(next__slide, 2000);
+
