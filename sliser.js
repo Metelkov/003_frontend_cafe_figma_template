d677b8b3ffd1db85d22f7291a05d5f670325c493
Alexey Avdeev <aad.jerry@gmail.com>
2020-11-19 19:57:28 +0300

    remove slider_buttons file

--- sliser.js
+++ sliser.js
@@ -78,4 +78,49 @@ function changeSlide() {
 
 */
 
- setInterval(changeSlide, 3500);
+setInterval(changeSlide, 3500);
+
+for (button of document.querySelectorAll('.slider-dots__common')) {
+  button.addEventListener('click', function(event) {
+      switch (event.target.innerText) {
+      case 'slide00':
+          //document.location.href = "#start__scroll";
+          //alert("нажата кнопка слайдера 1");
+  
+          document.getElementsByClassName('slide-00')[0].style.transition = 'transform 2s ease-out';
+          document.getElementsByClassName('slide-00')[0].style.transform = 'translateX(0px)';
+          document.getElementsByClassName('slide-01')[0].style.transform = 'translateX(1440px)';
+          document.getElementsByClassName('slide-02')[0].style.transform = 'translateX(1440px)';
+          document.getElementsByClassName('slide-03')[0].style.transform = 'translateX(1440px)';
+          wait2Sec();
+          //тут нужна функция останавливающая анимацию на 2 сек, но то что есть не так работает
+      break;
+      case 'slide01':
+          alert("нажата кнопка слайдера 2");
+          document.getElementsByClassName('slide-01')[0].style.transition = 'transform 2s ease-out';
+          document.getElementsByClassName('slide-01')[0].style.transform = 'translateX(0px)';
+          document.getElementsByClassName('slide-00')[0].style.transform = 'translateX(1440px)';
+          document.getElementsByClassName('slide-02')[0].style.transform = 'translateX(1440px)';
+          document.getElementsByClassName('slide-03')[0].style.transform = 'translateX(1440px)';
+          //тут нужна функция останавливающая анимацию на 2 сек
+      break;
+      case 'slide02':
+          alert("нажата кнопка слайдера 3");
+          document.getElementsByClassName('slide-02')[0].style.transition = 'transform 2s ease-out';
+          document.getElementsByClassName('slide-02')[0].style.transform = 'translateX(0px)';
+          document.getElementsByClassName('slide-00')[0].style.transform = 'translateX(1440px)';
+          document.getElementsByClassName('slide-01')[0].style.transform = 'translateX(1440px)';
+          document.getElementsByClassName('slide-03')[0].style.transform = 'translateX(1440px)';
+          //тут нужна функция останавливающая анимацию на 2 сек
+      break;
+      case 'slide03':
+          document.getElementsByClassName('slide-03')[0].style.transition = 'transform 2s ease-out';
+          document.getElementsByClassName('slide-03')[0].style.transform = 'translateX(0px)';
+          document.getElementsByClassName('slide-00')[0].style.transform = 'translateX(1440px)';
+          document.getElementsByClassName('slide-01')[0].style.transform = 'translateX(1440px)';
+          document.getElementsByClassName('slide-02')[0].style.transform = 'translateX(1440px)';
+          //тут нужна функция останавливающая анимацию на 2 сек
+      break;
+      }
+  });
+}
